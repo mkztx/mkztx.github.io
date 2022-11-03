24caea9aec1606299a5d319d995466a098c7ba2a
Maciej <m.rybkowski@icloud.com>
2022-11-03 22:21:05 +0100

    added dark mode, and hover for phone

--- script.js
+++ script.js
@@ -14,9 +14,24 @@ if (Math.floor(window.innerHeight) > Math.floor(window.innerWidth)) {
 	main.style.flexDirection = 'row';
 }
 
-const main = document.querySelector('.main');
-main.classList.add('mainDarkMode');
+// const main = document.querySelector('.main');
+// main.classList.add('mainDarkMode');
+// const box = document.querySelectorAll('.box');
+// box.forEach((box) => {
+// 	box.classList.add('boxDarkMode');
+// });
+
+//check for dark mode
 const box = document.querySelectorAll('.box');
 box.forEach((box) => {
-	box.classList.add('boxDarkMode');
+	box.setAttribute('aria-haspopup', 'true');
 });
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	console.log('dark mode');
+	const main = document.querySelector('.main');
+	main.classList.add('mainDarkMode');
+	const box = document.querySelectorAll('.box');
+	box.forEach((box) => {
+		box.classList.add('boxDarkMode');
+	});
+}
